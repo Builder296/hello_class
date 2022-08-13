85b32a879145d22ea7fca3fab4c751043591b7fe
builder <builder@odds.team>
2022-08-13 10:23:32 +0700

    Try palindrome

--- main.go
+++ main.go
@@ -3,38 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("power:", power(2, 5))
+	fmt.Println("isPalindrome:", isPalindrome(1, 2, 3, 3, 1))
 }
 
-func power(b, x int) int {
-	result := 1
-	for i := 0; i < x; i++ {
-		result *= b
-	}
-	return result
-}
-
-func mainBasic() {
-	println(squareArea(20))
-	fmt.Println("Aquare area of 3 is", squareArea(20))
-
-	a, b := swap(1, 5)
-	fmt.Println("Swap 1 and 2:", a, b)
-
-	if ok := IsCorrect(); ok {
-		println("It's correct")
-	}
-
-}
-
-func squareArea(a float64) float64 {
-	return a * a
-}
-
-func swap(a, b int) (int, int) {
-	return b, a
-}
-
-func IsCorrect() bool {
-	return true
+func isPalindrome(a, b, c, d, e int) bool {
+	return a == e && b == d
 }
