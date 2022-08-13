b0dea2e6abd294fd3f1513341e76bfc8d0b10f16
builder <builder@odds.team>
2022-08-13 10:17:40 +0700

    Add power function

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("power:", power(2, 5))
+}
+
+func power(b, x int) int {
+	result := 1
+	for i := 0; i < x; i++ {
+		result *= b
+	}
+	return result
+}
+
+func mainBasic() {
 	println(squareArea(20))
 	fmt.Println("Aquare area of 3 is", squareArea(20))
 
 	a, b := swap(1, 5)
 	fmt.Println("Swap 1 and 2:", a, b)
 
-	if ok := IsCorrect(); ok { // scope
+	if ok := IsCorrect(); ok {
 		println("It's correct")
 	}
-	{
-		a := 10
-		println("a:", a)
-	}
+
 }
 
 func squareArea(a float64) float64 {
