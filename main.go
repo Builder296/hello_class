36f7b07576076caf6a407ea648466fc681fc14eb
builder <builder@odds.team>
2022-08-13 13:11:22 +0700

    Solve multiply matrix value

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-var name string = "Builder"
+import "fmt"
 
 func main() {
-	n := os.Getenv("NAME")
-	if n != "" {
-		name = n
+	matrix := [4][4]int{
+		{1, 3, 4, 2},
+		{3, 4, 2, 1},
+		{4, 2, 1, 3},
+		{2, 1, 3, 4},
 	}
-	fmt.Println("Hello,", name)
+
+	r := multiply(matrix, 2)
+
+	for _, rr := range r {
+		fmt.Println(rr)
+	}
+}
+
+func multiply(matix [4][4]int, n int) [4][4]int {
+	var result [4][4]int
+	for i, nSet := range matix {
+		var resultN [4]int
+		for j, num := range nSet {
+			resultN[j] = num * n
+		}
+		result[i] = resultN
+	}
+	return result
 }
