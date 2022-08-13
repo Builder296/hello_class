489b5a2e607be3335299db8995fc7cf8e787fbaa
builder <builder@odds.team>
2022-08-13 13:52:53 +0700

    Slice of Rune vs String

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ func main() {
 	// "abcdefg" -> []string{"ab", "cd", "ef", "g*"}
 	fmt.Println(couple("abcdef"))
 	fmt.Println(couple("abcdefg"))
+	fmt.Println(couple("สวัสดีครับ"))
 }
 
-func couple(s string) []string {
+func couple(str string) []string {
 	var result []string
-
-	for s += "*"; len(s) > 1; s = s[2:] {
-		result = append(result, s[:2])
+	s := []rune(str)
+	for s = append(s, []rune("*")...); len(s) > 1; s = s[2:] {
+		result = append(result, string(s[:2]))
 	}
 
 	return result
