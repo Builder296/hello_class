d6ad0e506c6e4bed57e88cac3ed3efb9069a9a37
builder <builder@odds.team>
2022-08-13 11:18:25 +0700

    Refactor code

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -3,23 +3,14 @@ package foobar
 import "strconv"
 
 func Say(n int) string {
-	if n == 7 {
-		return strconv.Itoa(n)
-	}
 	if n == 5 {
 		return "bar"
-	}
-	if n == 4 {
-		return strconv.Itoa(n)
 	}
 	if n == 6 {
 		return "foo"
 	}
 	if n == 3 {
 		return "foo"
-	}
-	if n == 2 {
-		return strconv.Itoa(n)
 	}
 	return strconv.Itoa(n)
 }
