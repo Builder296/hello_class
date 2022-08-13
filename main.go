8042333d1f8c5033170664f1e0fe7bcf6623d30c
builder <builder@odds.team>
2022-08-13 10:02:10 +0700

    Can return more than one

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import "fmt"
 
+func main() {
+	println(squareArea(20))
+	fmt.Println("Aquare area of 3 is", squareArea(20))
+
+	a, b := swap(1, 5)
+	fmt.Println("Swap 1 and 2:", a, b)
+}
+
 func squareArea(a float64) float64 {
 	return a * a
 }
 
-func main() {
-	println(squareArea(20))
-	fmt.Println(squareArea(20))
+func swap(a, b int) (int, int) {
+	return b, a
 }
