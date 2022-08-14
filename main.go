e5e79ce294ef93805bb3af075da4a9d82f369c31
builder <builder@odds.team>
2022-08-14 09:35:39 +0700

    Try to connect postgres

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"strings"
+	"log"
+
+	"github.com/jackc/pgx/v4"
 )
 
-type String string
-
-func (s *String) toUpper() {
-	*s = String(strings.ToUpper(string(*s)))
-}
-
 func main() {
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:mysecretpassword@localhost:5432/myapp")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	var a String = "abc"
-	a.toUpper()
-	fmt.Println(a)
+	defer conn.Close(context.Background())
+
+	if _, err := conn.Exec(context.Background(), "INSERT INTO TODOS(TASK) VALUES($1)", "Hello db"); err != nil {
+		// Handling error, if occur
+		fmt.Println("Unable to insert due to: ", err)
+		return
+	}
+	fmt.Println("Insertion Succesfull")
 }
