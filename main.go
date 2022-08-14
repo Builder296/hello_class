4c5eb2708b049be4845d6a36be62462bfff5a2e0
builder <builder@odds.team>
2022-08-14 10:07:02 +0700

    Add handler

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -16,10 +18,33 @@ func main() {
 
 	defer conn.Close(context.Background())
 
-	if _, err := conn.Exec(context.Background(), "INSERT INTO TODOS(TASK) VALUES($1)", "Hello db"); err != nil {
-		// Handling error, if occur
-		fmt.Println("Unable to insert due to: ", err)
+	// if _, err := conn.Exec(context.Background(), "INSERT INTO TODOS(title) VALUES($1)", "Hello db"); err != nil {
+	// 	// Handling error, if occur
+	// 	fmt.Println("Unable to insert due to: ", err)
+	// 	return
+	// }
+	// fmt.Println("Insertion Succesfull")
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/todos", todoNewTaskHandler)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+type todoNewTask struct {
+	Title string `json:"title"`
+}
+
+func todoNewTaskHandler(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var t todoNewTask
+	err := decoder.Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Println("Insertion Succesfull")
 }
