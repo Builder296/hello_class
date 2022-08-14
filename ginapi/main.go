d05451894b8ce8046281f39cb382daf6bd897eec
builder <builder@odds.team>
2022-08-14 13:26:06 +0700

    Add great shutdown

--- ginapi/main.go
+++ ginapi/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	srv := &http.Server{
+		Addr:    ":8081",
+		Handler: r,
+	}
+
+	go func() { // using go bacase ListenAndServe is infinity it will block!
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+
+	log.Println("Server exiting")
 }
